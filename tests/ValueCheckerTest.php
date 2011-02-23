1cd05b964fc1c04f9e65492673452213c56b080d
Nuzhdin Urii aka tz-lom <nuzhdin.urii@gmail.com>
2011-02-23 22:38:17 +0300

    Добавлены тесты для проверки значений,требуется покрытие кодом

---
+++ tests/ValueCheckerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once './autoload.php';
+
+class Checker extends \TrustedForms\ValueChecker
+{
+	protected function performCheck($value)
+	{
+		return $value!=false;
+	}
+}
+
+
+class ValueCheckerTest extends PHPUnit_Framework_TestCase
+{
+	public function testReporterAssignment()
+	{
+		$reporter = new \TrustedForms\ErrorReporter('Error occured');
+		$checker = new Checker();
+		$checker->setReporter($reporter);
+
+		$this->assertEquals($reporer,$checker->getReporter());
+	}
+
+	public function testSimpleCheck()
+	{
+		$checker = new Checker();
+		$this->assertEquals(true,$checker->check(true));
+		$this->assertEquals(false,$checker->check(false));
+	}
+
+}
+
---
+++ tests/ValueChecks/IsNumericTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../autoload.php';
+
+class IsNumericTest extends PHPUnit_Framework_TestCase
+{
+	protected function testChecker()
+	{
+		$checker = new \TrustedForms\ValueChecks\IsNumeric();
+		
+		$this->assertEquals(true,$checker->check('42'));
+		$this->assertEquals(false,$checker->check('abc'));
+	}
+
+}
+
