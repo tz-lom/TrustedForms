9b3ec4b40896fb6ee7d12409c5257334750b076b
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2011-03-04 23:20:09 +0300

    Убрал ненужную информацию

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,7 +3,6 @@
 spl_autoload_register(function($classname){
 	if(strpos($classname,'TrustedForms\\')===0)
 	{
-		echo __DIR__.'/../library/'.str_replace('\\', '/', substr($classname, 13)).'.php',"\n";
 		require_once __DIR__.'/../library/'.str_replace('\\', '/', substr($classname, 13)).'.php';
 	}
 });
