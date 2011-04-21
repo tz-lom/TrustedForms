f992567386466003a4cfdcbf46fa762c37078e78
Nuzhdin Urii aka tz-lom <nuzhdin.urii@gmail.com>
2011-04-22 00:55:23 +0400

    Изменение в автолоадере (кросс платформенность)

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,6 +3,6 @@
 spl_autoload_register(function($classname){
 	if(strpos($classname,'TrustedForms\\')===0)
 	{
-		require_once __DIR__.'/../library/'.str_replace('\\', '/', substr($classname, 13)).'.php';
+		require_once __DIR__.'/../library/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($classname, 13)).'.php';
 	}
 });
