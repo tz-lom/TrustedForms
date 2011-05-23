0652ef5f0289d8c7348523b6470bf82a7d4f468e
Nuzhdin Urii aka tz-lom <nuzhdin.urii@gmail.com>
2011-05-24 00:32:23 +0400

    Added license info

--- library/CodeGenerator/TemplateManipulator.php
+++ library/CodeGenerator/TemplateManipulator.php
@@ -1,11 +1,14 @@
 <?php
+/**
+ * @version 0.0.1
+ * @link http://github.com/tz-lom/TrustedForms
+ * @author Nuzhdin Urii <nuzhdin.urii@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @package TrustedForms\CodeGenerator
+ */
 
 namespace TrustedForms\CodeGenerator;
 
-/**
- *
- * @author tz-lom
- */
 interface TemplateManipulator
 {
 
