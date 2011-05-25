35dbc290b330cee33cc943c23c2497cb646f63f8
prowoke <prowoke@rambler.ru>
2011-05-25 20:45:56 +0400

    Refresh email validator

--- library/ValueChecks/isEmail.php
+++ library/ValueChecks/isEmail.php
@@ -11,6 +11,6 @@ class isEmail extends \TrustedForms\ValidationChainItem
      */
     protected function doProcess(&$value)
 	{
-        return (bool)(preg_match("/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/", $value));
+        return (bool)(filter_var($value, FILTER_VALIDATE_EMAIL));
     }
 }
