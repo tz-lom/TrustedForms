a9dd04cd073f0e61b9c2e9b9b7fc495b2046ee74
Nuzhdin Urii aka tz-lom <nuzhdin.urii@gmail.com>
2011-06-03 21:14:41 +0400

    Added easy-build debug environment for JS code

---
+++ selenium/environment/debugJS.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <style type="text/css">
+        .error { border: 1px solid red; }
+        </style>
+		<script type="text/javascript" src="jquery.js"></script>
+        <script type="text/javascript" src="js/TrustedForms.js"></script>
+    </head>
+    <body>
+        <?php 
+        require_once('../../tests/autoload.php');
+        include 'debugJS.validate.php';
+        include 'debugJS.tpl';
+        ?>
+    </body>
+</html>
